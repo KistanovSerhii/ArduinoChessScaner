b3a2c704078b50bba194be755e6dfa113715c840
KistanovSerhii <Kistanov.serhii@yandex.ru>
2022-10-07 10:32:00 +0300

    add unsigned long

--- Chess/Chess.ino
+++ Chess/Chess.ino
@@ -1,10 +1,15 @@
 #include <SoftwareSerial.h>
 
+// Bluetooth name "CHESS_BOARD", pass 1234
+
 SoftwareSerial HC_05(10, 11); // RX | TX (D10, D11)
 
+unsigned long BoardSide1;  //A1 - первый бит, H4 - последний бит. 32 поля так как 4 байта.
+unsigned long BoardSide2;  //A5 - первый бит, H8 - последний бит. 32 поля так как 4 байта.
+
+// число 255 - это 1111 1111 (все поля первой вертикали заняты)
+
 int randNumber;
-char randNumberChar[2];
-String randNumberStr;
 
 byte outputData;
 byte inputData;
@@ -15,6 +20,7 @@ void setup()
   Serial.begin(9600);
   HC_05.begin(9600);  //Default Baud for comm, it may be different for your Module. 
   Serial.println("The bluetooth is ready to pairing!");
+  
 }
  
 void loop()
@@ -32,14 +38,8 @@ void loop()
     inputStr.trim();
     if (inputStr == "test")
     {
-      randNumber = random(1, 8);
-
-      // https://geekmatic.in.ua/preobrazovanie_char_v_integer
-      randNumberStr = String(randNumber);
-      randNumberStr.toCharArray(randNumberChar, 2);
-           
-      sendDataForBluetooth(randNumber);
-      HC_05.write(randNumber);
+      randNumber = random(1, 25);      
+      sendDataForBluetooth(randNumber); // number is converting to byte and sending as char (alphabet) A = 1, B = 2, C = 3 ...;
     }
     
   }
