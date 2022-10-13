73232211afa9d472aba860a72fa095b11aa566b8
KistanovSerhii <Kistanov.serhii@yandex.ru>
2022-10-13 16:02:30 +0300

    befor add non-null

--- flutter/lib/internal/application.dart
+++ flutter/lib/internal/application.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 import '../presentation/home.dart';
 
+import 'package:flutter_blue/flutter_blue.dart';
+import '../presentation/bluetooth_off_screen.dart';
+import '../presentation/find_devices_screen.dart';
+
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart chess board',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: StreamBuilder<BluetoothState>(
+          stream: FlutterBlue.instance.state,
+          initialData: BluetoothState.unknown,
+          builder: (c, snapshot) {
+            final state = snapshot.data;
+            if (state == BluetoothState.on) {
+              return FindDevicesScreen();
+            }
+            return BluetoothOffScreen(state: state);
+          }),
+      // Home(),
     );
   }
 }
